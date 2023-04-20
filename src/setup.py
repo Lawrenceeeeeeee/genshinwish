aef72efb34086c1e5f005871ef010b291b2f36ed
Lawrence Guo <125617257+Lawrenceeeeeeee@users.noreply.github.com>
2023-04-20 18:35:39 +0800

    Add files via upload

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="src",
+    version="1.0",
+    author="Lawrence Guo",
+    author_email="775569157@qq.com",
+    description="",
+    long_description="src",
+    long_description_content_type="src",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
